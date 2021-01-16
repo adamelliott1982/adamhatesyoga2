84de994bec0bfa5f2226ac7e2047e6e0b5118264
Adam Elliott <adamelliott1982@gmail.com>
2021-01-16 16:55:03 -0600

    updates 1/16/2021

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
+
+
+
+
+
+
+//////////////////////////////////////// NAV ANIMATIONS
 const circle = document.querySelector('.circle');
 const main = document.querySelector('main');
-const imageBoxes = document.querySelectorAll('.image-box');
-
 
 circle.addEventListener('click', ()=>{
     circle.classList.toggle('nav-open');
     main.classList.toggle('nav-open');
 })
 
+
+
+
+
+//////////////////////////////////////// GALLERY PAGE
+const imageBoxes = document.querySelectorAll('.image-box');
+const imageNum = document.getElementById('image-num')
+
+imageBoxes.forEach((imageBox) => {
+    imageBox.addEventListener('click', ()=>{
+        removeActiveClasses()
+        imageBox.classList.add('active')
+    })
+})
+
+
+function removeActiveClasses(){
+    imageBoxes.forEach(imageBox => {
+        imageBox.classList.remove('active')
+    });
+}
+
+
+
+//////////////////////////////////////// CONTACT US PAGE
+const questionButton = document.getElementById('question-button')
+const submitButton = document.getElementById('submit-button')
+const formText = document.getElementById('form-text-area')
+
+questionButton.addEventListener('click', ()=>{
+    formText.classList.add('active')
+    submitButton.classList.add('active')
+})
